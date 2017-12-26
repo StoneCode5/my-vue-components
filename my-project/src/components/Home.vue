1bead5574ec39528b4ac14df1b584284da53b69c
PC-201703251404\Administrator <751380551@qq.com>
2017-12-26 14:40:35 +0800

    增加一个对图片居中处理的组件

---
+++ my-project/src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+        <centerShowImg></centerShowImg> 
+  </div>
+</template>
+
+<script>
+import centerShowImg from './centerShowImg'
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    centerShowImg
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ my-project/src/components/centerShowImg.vue
@@ -0,0 +1,79 @@
+<!-- 对于微信九宫格的图片居中显示处理 -->
+
+<style scoped>
+  .imgBox{
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .imgItemBox{
+    width:25vw;
+    height:25vw;
+    overflow: hidden;
+  }
+  /* 如果不考虑兼容性,可以直接使用object-fit:cover;来达到效果.*/
+  /* .imgItem{
+    width: 100%; height: 100%;    
+    object-fit: cover;
+  } */
+</style>
+<template>
+  <div class="centerShowImg">
+    <div class="imgBox">
+      <div class="imgItemBox" v-for="(item, index) in imgArr" :key="index">
+        <img :src="item.url" alt="" class="imgItem" ref="imgList">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      imgArr: [
+
+      ]
+    }
+  },
+  created() {
+    // 必须要在created里面来写入数据,因为mounted里面已经开始挂载数据了.
+    this.imgArr = [
+      {url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1514269362763&di=cbdc9b8564041a075898a69c1250a3c4&imgtype=0&src=http%3A%2F%2Fimgstore.cdn.sogou.com%2Fapp%2Fa%2F100540002%2F604382.jpg'},
+      {url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1514276975248&di=ea6c6656245e62a87e7c2cbb538a03ae&imgtype=0&src=http%3A%2F%2Fuploads.xuexila.com%2Fallimg%2F1706%2F28-1F62Q22313.jpg'}
+    ]
+  },
+  mounted() {
+    const that = this
+    let imgList = this.$refs.imgList
+    imgList.forEach(function (v) {
+        v.onload = function () {
+          that.updateImgPosition(v)   
+        } 
+      })
+  },
+  methods: {
+    updateImgPosition (v) {
+    let imgWidth = v.width
+    let imgHeight = v.height
+    let imgBoxWidth = window.innerWidth * 0.25 // window.innerWidth是可视窗口的宽度.
+    let imgBoxHeight = imgBoxWidth
+    if (imgWidth > imgHeight) {
+      v.style.height = '100%'
+      let heightRate = (imgBoxHeight / imgHeight)
+      let leftNum = ((imgWidth * heightRate) - imgBoxWidth) / 2
+      v.style.margin = `0 0 0 ${-leftNum}px`
+    } else if (imgWidth < imgHeight) {
+      v.style.width = '100%'
+      let widthRate = (imgBoxWidth / imgWidth)
+      let topNum = ((imgHeight * widthRate) - imgBoxHeight) / 2
+      v.style.margin = `${-topNum}px 0 0 0 `
+    } else if (imgWidth === imgHeight) {
+      v.style.width = '100%'
+      v.style.height = '100%'
+    }
+  }
+  }
+}
+</script>
+
+
